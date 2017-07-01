8b1154188f9e27604fe88c6cc9de0163deb07cf5
Tim Campbell <tc.campbell@gmail.com>
2017-07-01 14:02:58 +1000

    Halfway through the Replay script

---
+++ Assets/Player/Replay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Replay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class MyKeyFrame {
+	public float time;
+	public Vector3 pos;
+
+}
+
+
+//Time- Float
+//pos- Vector 3
+//rot - quaternion
