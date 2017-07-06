220eeb993f93abd3902f5d27ef132788fc4aee61
Tim Campbell <tc.campbell@gmail.com>
2017-07-06 21:49:14 +1000

    Lesson on Struct vs Class

--- Assets/Player/Replay.cs
+++ Assets/Player/Replay.cs
@@ -6,7 +6,7 @@ public class Replay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		MyKeyFrame testKey = new MyKeyFrame(1.0f, Vector3.up, Quaternion.identity);
 	}
 	
 	// Update is called once per frame
@@ -15,9 +15,20 @@ public class Replay : MonoBehaviour {
 	}
 }
 
-public class MyKeyFrame {
-	public float time;
-	public Vector3 pos;
+
+//A struct is stored as a value type rather than a reference
+public struct MyKeyFrame {
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	//The constructor has the same name as the class
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation){ //THE CONSTRUCTOR
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+
+	}
 
 }
 
